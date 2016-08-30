76b8b3259c58daca3b7b52627dad09613fbdb88e
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-08-29 21:31:36 -0300

    Implementing tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.Hspec
+import Feynman
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Feynman" $ do
+    it "Testing 1x1 square." $ do
+      feynman 1 `shouldBe` 1
+
+    it "Testing 2x2 square." $ do
+      feynman 2 `shouldBe` 5
+
+    it "Testing 8x8 square." $ do
+      feynman 8 `shouldBe` 204
+
